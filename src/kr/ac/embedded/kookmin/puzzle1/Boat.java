01bc4b33c16edff336c7beb942848408842b7bb4
jaehyoung <xxf1234@naver.com>
2015-09-29 18:53:45 +0900

    20092310 수학과 이재형  모바일프로그래밍 과제

--- src/kr/ac/embedded/kookmin/puzzle1/Boat.java
+++ src/kr/ac/embedded/kookmin/puzzle1/Boat.java
@@ -1,16 +1,19 @@
 package kr.ac.embedded.kookmin.puzzle1;
 
+
+
 public class Boat {
+	
 	private int length ;
 	public void setLength ( int len ) {
 		length = len;
 	}
 	
 	public int getLength() {
-		  /** **/
+		return length;
 	}
 	
 	public void move() {
-		System.out.print(/** **/);
+		System.out.print("drift");
 	}
 }
--- src/kr/ac/embedded/kookmin/puzzle1/TestBoats.java
+++ src/kr/ac/embedded/kookmin/puzzle1/TestBoats.java
@@ -6,7 +6,7 @@ public class TestBoats {
 		Sailboat  b2 = new Sailboat();
 		Rowboat b3 = new Rowboat();
 		b2.setLength(32);
-		b1.move();
+		b1.move(); 
 		b3.move();
 		b2.move();
 	}
